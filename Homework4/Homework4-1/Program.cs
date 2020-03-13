eb30db18a7f169acdc58218318be26174ee53586
Zhuyuhan2333 <1245231959@qq.com>
2020-03-13 17:40:44 +0800

    homework for week4

---
+++ Homework4/Homework4-1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4_1
+{
+    class Node<T> {
+        public Node<T> Next { set; get; }
+        public T data { set; get; }
+        public Node(T t){
+            this.data = t;
+            this.Next = null;
+       }
+    } 
+    class ArrayList<T>
+    {
+        public Node<T> head;
+        public Node<T> tail;
+        public ArrayList()
+        {
+            head = tail = null;
+        }
+        public void add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                tail = head = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        //参数为委托：该委托为参数为T的方法
+        public void ForEach(Action<T> action)
+        {
+            //从头开始遍历
+            Node<T> t = this.head;
+            while (t != null)
+            {
+                Console.WriteLine(t.data);
+                //委托申明：将要对t.data进行其他操作
+                action(t.data);
+                t = t.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            ArrayList<int> myList = new ArrayList<int>();
+            myList.add(5);
+            myList.add(10);
+            int max = myList.head.data;
+            int min = myList.head.data;
+            Action<int> action = s=>sum += s;
+            action += (s => { if (s > max) max = s; });
+            action += (s => { if (s < min) min = s; }); ;
+            myList.ForEach(action);
+            Console.WriteLine($"{max}+{min}+{sum}");
+        }
+    }
+}
---
+++ Homework4/Homework4-2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Homework4_2
+{
+    class Time{
+        private int second;
+        private int minute;
+        private int hour;
+        public int S
+        {
+            set => second = value;
+            get => second;
+        }
+        public int M
+        {
+            set => minute = value;
+            get => minute;
+        }
+        public int H
+        {
+            set => hour = value;
+            get => hour;
+        }
+    }
+    class Clock
+    {
+        //事件申明及触发类
+        public event Action<object> Tick;
+        public event Action<object,Time> Alarm;
+        public Time current = new Time();
+        public Time alarmTime = new Time();
+        public void setAlarmTime(Time t)
+        {
+            alarmTime.S = t.S;
+            alarmTime.H = t.H;
+            alarmTime.M = t.M;
+        }
+        public void setClock()
+        {
+            current.S = DateTime.Now.Second;
+            current.M = DateTime.Now.Minute;
+            current.H = DateTime.Now.Hour;
+            Tick(this);
+            Alarm(this, alarmTime);
+        }
+    }
+    class ClockHandler
+    {
+        public Clock cl = new Clock();
+        //实现事件绑定
+        public ClockHandler() {
+            cl.Tick += this.getTick;
+            cl.Alarm += this.getAlarm;
+        }
+        public void getTick(Object sender)
+        {
+            Console.WriteLine("tick");
+        }
+        public void getAlarm(object sender,Time t)
+        {
+            if (cl.current.H == t.H && cl.current.M == t.M && cl.current.S == t.S)
+            {
+                Console.WriteLine("ring");
+            }
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+            ClockHandler clh = new ClockHandler();
+            Time alTime = new Time();
+            alTime.H = 17;
+            alTime.M = 33;
+            alTime.S = 0;
+            clh.cl.setAlarmTime(alTime);
+            while (i < 200)
+            {
+                clh.cl.setClock();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
